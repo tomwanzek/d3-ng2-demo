67d9c99b11548c24967ca52318922ca53500488b
Tom Wanzek <tomwanzek@gmail.com>
2016-09-18 15:38:34 -0400

    Initial changes to e2e tests.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,61 @@
 import { D3Ng2DemoPage } from './app.po';
 
-describe('d3-ng2-demo App', function() {
+import { browser } from 'protractor/globals';
+
+describe('d3-ng2-demo App', function () {
   let page: D3Ng2DemoPage;
 
   beforeEach(() => {
     page = new D3Ng2DemoPage();
   });
 
-  // it('should display message saying D3 Demo', () => {
-  //   page.navigateTo();
-  //   expect(page.getParagraphText()).toEqual('D3 Demo');
-  // });
+  it('should display message saying Visual Power at Scale?', () => {
+    // HACK: to avoid synchronization error
+    browser.ignoreSynchronization = true;
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Visual Power at Scale?');
+  });
+
+  it('should have one svg with dimensions 960x600 in app-brush-zoom-2 component', () => {
+    // HACK: to avoid synchronization error
+    browser.ignoreSynchronization = true;
+    page.navigateTo();
+    let svg = page.getComponentElement('app-brush-zoom-2', 'svg');
+    expect(svg.count()).toEqual(1);
+    expect(svg.getAttribute('width')).toEqual('960');
+    expect(svg.getAttribute('height')).toEqual('600');
+
+  });
+
+  it('should have one svg with dimensions 400x400 in app-drag-zoom-2 component', () => {
+    // HACK: to avoid synchronization error
+    browser.ignoreSynchronization = true;
+    page.navigateTo();
+    let svg = page.getComponentElement('app-drag-zoom-2', 'svg');
+    expect(svg.count()).toEqual(1);
+    expect(svg.getAttribute('width')).toEqual('400');
+    expect(svg.getAttribute('height')).toEqual('400');
+
+  });
+
+  it('should have one canvas with dimensions 400x400 in app-voronoi-spirals-3 component', () => {
+    // HACK: to avoid synchronization error
+    browser.ignoreSynchronization = true;
+    page.navigateTo();
+    let svg = page.getComponentElement('app-voronoi-spirals-3', 'canvas');
+    expect(svg.count()).toEqual(1);
+    expect(svg.getAttribute('width')).toEqual('400');
+    expect(svg.getAttribute('height')).toEqual('400');
+  });
+
+  it('should resize the layout of the Drag & Zoom II visual', () => {
+    // HACK: to avoid synchronization error
+    browser.ignoreSynchronization = true;
+    page.navigateTo();
+    let button = page.getLayoutButton('large');
+    button.click().then(function () {
+      // expect(page.getComponentElement('app-drag-zoom-2', 'svg').getAttribute('width')).toEqual('600');
+      // expect(page.getComponentElement('app-drag-zoom-2', 'svg').getAttribute('height')).toEqual('600');
+    });
+  });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,21 @@ export class D3Ng2DemoPage {
     return browser.get('/');
   }
 
-  // getParagraphText() {
-  //   return element(by.css('app-root h1')).getText();
-  // }
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+
+  getComponentElement(componentSelector: string, elementCssSelector: string) {
+    return element(by.tagName(componentSelector)).all(by.tagName(elementCssSelector));
+  }
+
+  getComponentElementCount(componentSelector: string, elementCssSelector: string) {
+    return element(by.tagName(componentSelector)).all(by.tagName(elementCssSelector)).count();
+  }
+
+
+  getLayoutButton(buttonText: string) {
+    return element(by.tagName('app-wrapper-drag-zoom-2')).all(by.tagName('button')).all(by.xpath('.//*[.="' + buttonText + '"]'));
+  }
+
 }
