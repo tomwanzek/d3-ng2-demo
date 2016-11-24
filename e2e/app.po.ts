ce9cf36f42c96871cd15e735246690e09c4884b3
Tom Wanzek <tomwanzek@gmail.com>
2016-11-24 14:10:13 -0500

    Ugrade dependencies: * Use angular-cli beta.21 * Use d3-ng2 ^1.4

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,17 +1,11 @@
-import { browser, element, by } from 'protractor/globals';
+import { browser, element, by } from 'protractor';
 
 export class D3Ng2DemoPage {
   navigateTo() {
     return browser.get('/');
   }
 
-
-  getComponentElement(componentSelector: string, elementCssSelector: string) {
-    return element(by.tagName(componentSelector)).all(by.tagName(elementCssSelector));
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
   }
-
-  getLayoutButton(buttonText: string) {
-    return element(by.tagName('app-wrapper-drag-zoom-2')).all(by.css('button.layout-' + buttonText));
-  }
-
 }
